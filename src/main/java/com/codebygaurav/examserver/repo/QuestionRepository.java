66dc8fe2523f5a5cecc315857e73877f8eb30ff3
Gaurav Sharma <gauravsharma_2icloud.com@Gauravs-MacBook-Air.local>
2021-06-02 14:54:08 +0530

    Mapping and delete problem resolve

--- src/main/java/com/codebygaurav/examserver/repo/QuestionRepository.java
+++ src/main/java/com/codebygaurav/examserver/repo/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.codebygaurav.examserver.repo;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.codebygaurav.examserver.entity.exam.Question;
 import com.codebygaurav.examserver.entity.exam.Quiz;
